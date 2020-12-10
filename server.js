18ffcf1359283cc47a384a2acf8637f95330fd53
Dio <diooktar@gmail.com>
2020-12-11 02:54:56 +0700

    minor

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
-
 const app = express()
-
-app.use('/', serverStatic(path.join(__dirname,'/dist')))
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 const port = process.env.PORT || 8080
 app.listen(port)
